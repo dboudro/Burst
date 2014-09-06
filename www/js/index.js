446c086dbffd86dac2f981d28094eca46c4655e9
deep <deeep.deep@gmail.com>
2014-09-06 15:39:12 -0400

    sample audio frmo server

--- www/js/index.js
+++ www/js/index.js
@@ -4,5 +4,15 @@ var mediaFile;
 function l(){console.log(arguments);}
 
 document.getElementById("record").onclick = function () {
-  record_parse_file().then(l,l);
+  record_parse_file()
+  .then(function(parseFile){
+    console.log("got parse file: ". parseFile);
+    return parseFile.save();
+  },l)
+  .then(function(savedFile) {
+    console.log("got saved file: ", savedFile);
+    var clip = new Parse.Object("Clip");
+    clip.set("wav", savedFile);
+    clip.save();
+  });
 };
